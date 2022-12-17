3d27e9c1d88485c41fb51e02b82b3176093f21d8
Alex Van Vleet <alexvan73@yahoo.com>
2022-12-16 17:03:55 -0700

    .

---
+++ client/src/ForumSelector.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+
+export default function ForumSelector({ countries }) {
+  const [forums, setForums] = useState([]);
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/forums/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setForums(data);
+      });
+  }, [id]);
+
+  {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {countries.map((country) => {
+          return (
+            <table>
+              <tr onClick>{country.name}</tr>
+            </table>
+          );
+        })}
+      </div>
+    );
+  }
+}
