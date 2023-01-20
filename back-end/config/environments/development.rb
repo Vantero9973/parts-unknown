a4f4adf761291406e0185bb100a80bd3eca3ff23
Seth Vergeyle <github@vergeyle.net>
2023-01-19 18:51:38 -0700

    fix: domain

--- back-end/config/environments/development.rb
+++ back-end/config/environments/development.rb
@@ -68,5 +68,8 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.hosts << "pu.vergeyle.net"
+  config.hosts = [
+    "pu.vergeyle.net",
+    "parts.undefined.vantero.dev"
+  ]
 end
